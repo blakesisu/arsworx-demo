d9d3ee116472f1d3b2c64e9c9c59ce0e3f873833
Blake Watkins <iamsoblake@gmail.com>
2018-08-20 12:27:08 -0700

    first commit

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,42 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+// Components
+// import ErrorBoundary from 'components/_shared/ErrorBoundary/ErrorBoundary';
+// SVGs
+// Constants
+
+export default class Settings extends React.Component {
+  // Prop Types
+  // ------------------------------------------------------------------------ //
+  static propTypes = {};
+  static defaultProps = {};
+
+  // Constructor
+  // ------------------------------------------------------------------------ //
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  // Variables
+  // ------------------------------------------------------------------------ //
+
+  // Lifecycle methods
+  // ------------------------------------------------------------------------ //
+
+  // Event handlers
+  // ------------------------------------------------------------------------ //
+
+  // Class methods
+  // ------------------------------------------------------------------------ //
+
+  // Render methods
+  // ------------------------------------------------------------------------ //
+  render() {
+    return (
+      <div className="settings">
+        <h1>This is the settings page</h1>
+      </div>
+    );
+  }
+}
